323d79e7867e1730c0f96b926d3514b6674a7c20
asoknaren <naren@prophoenix.com>
2026-05-24 17:29:12 -0400

    interactive widgets

---
+++ pages/subpage1_organized.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("Hello Streamlit-er 👋")
+
+
+st.text("sub page 1")
+st.markdown("_Markdown_")
+
+st.page_link("2intractivewidgets _Oraganized.py", label="Go to interactive widgets demo page")
+
+
+if st.button("Send balloons!"):
+    st.balloons()
