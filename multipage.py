f486d5d231ad5fec146111fec790673859fb079d
asoknaren <naren@prophoenix.com>
2026-05-24 16:45:44 -0400

    docker deployment added

--- multipage.py
+++ multipage.py
@@ -4,9 +4,19 @@ import streamlit as st
 main_page = st.Page("main_page.py", title="Main Page", icon="🎈")
 page_2 = st.Page("page_2.py", title="Page 2", icon="❄️")
 page_3 = st.Page("page_3.py", title="Page 3", icon="🎉")
+first_stream = st.Page("firststream.py", title="First Stream", icon="🧪")
+full_app = st.Page("zfullapp.py", title="Uber Demo", icon="🚕")
+progress_page = st.Page("progress_test.py", title="Progress Test", icon="⏳")
 
 # Set up navigation
-pg = st.navigation([main_page, page_2, page_3])
+pg = st.navigation([
+	main_page,
+	page_2,
+	page_3,
+	first_stream,
+	full_app,
+	progress_page,
+])
 
 # Run the selected page
 pg.run()
