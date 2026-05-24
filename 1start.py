e5ffefc8183d9c41ca719901fff79234e7e7e2a6
asoknaren <naren@prophoenix.com>
2026-05-24 17:00:40 -0400

    initial commands

--- 1start.py
+++ 1start.py
@@ -13,5 +13,17 @@ st.markdown(
     """
 )
 
+
+st.text("Fixed width text")
+st.markdown("_Markdown_")
+st.latex(r""" e^{i\pi} + 1 = 0 """)
+st.title("My title")
+st.header("My header")
+st.subheader("My sub")
+st.code("for i in range(8): foo()")
+st.badge("New")
+st.html("<p>Hi!</p>")
+st.iframe("https://www.microsoft.com", height=600)
+
 if st.button("Send balloons!"):
     st.balloons()
